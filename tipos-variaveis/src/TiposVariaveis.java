3e2856f1587e2ae4c486305c0171b6ed1f4074d0
cristielle <cristielle.santos94@gmail.com>
2023-10-10 17:27:16 -0300

    inserindo exemplos de tipos de dados

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,29 @@
 public class TiposVariaveis {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+        //tipos primitivos de dados
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+        long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+        float pi = 3.14F;
+        double salario = 1275.33;
+        //Observe que o tipo long precisa terminar com L, o tipo float precisa terminar com F 
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+        //Por mais que temos ciência que valor de numeroNormal cabe é um short, o Java não permite correr o risco.
+
+		//ao usar a palavra reservada final, você determina que jamais esta variavel poderá obter outro valor;
+		//isso é considerado uma CONSTANTE na linguagem Java
+	
+		final double VALOR_DE_PI = 3.14;
+		
+        VALOR_DE_PI=3.15; //Esta linha vai apresentar erro de compilação!
+    } 
+    
 }
+
+
