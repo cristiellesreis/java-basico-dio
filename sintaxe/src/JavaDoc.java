57730ed7ea78f1aa8941f577c3271fdfe9bde4bb
cristielle <cristielle.santos94@gmail.com>
2023-10-10 20:25:56 -0300

    criando arquivo javadoc

---
+++ sintaxe/src/JavaDoc.java
@@ -0,0 +1,12 @@
+public class JavaDoc {
+    public static void main(String[] args) {
+        //Javadoc
+
+        // No terminal execute o comando abaixo
+
+        //javadoc -encoding UTF-8 -docencoding ISO-8859-1  -d ../docs  src/*.java
+
+
+    }
+    
+}
