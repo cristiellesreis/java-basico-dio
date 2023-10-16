3b257627679cfca44409905a17f135c496e316bf
cristielle <cristielle.santos94@gmail.com>
2023-10-16 16:33:04 -0300

    criando arquivos sobre estruturas

---
+++ estruturas-condicionais/src/ExemploBreakContinue.java
@@ -0,0 +1,23 @@
+public class ExemploBreakContinue {
+    public static void main(String[] args) {
+
+        //Break
+        for(int numero = 1; numero <=5; numero ++){
+            if(numero==3)
+                break;
+            
+            System.out.println(numero);
+        }
+        
+        //Qual a saída no console ?
+
+        //Continue
+        /* 
+        for(int numero = 1; numero <=5; numero ++){
+            if(numero==3)
+                continue;
+            
+            System.out.println(numero);
+        */
+    }
+}
---
+++ estruturas-condicionais/src/ResultadoEscolar.java
@@ -0,0 +1,34 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+
+        /*condicional composta
+
+        int nota = 6;
+        
+        if(nota >= 7)
+         System.out.println("Aprovado");
+ 
+        else
+         System.out.println("Reprovado");
+        */
+
+        /*//condicionais encadeadas
+        int nota = 6;
+
+	    if (nota >= 7)
+		    System.out.println("Aprovado");
+	    else if (nota >= 5 && nota < 7)
+		    System.out.println("Recuperação");
+	    else
+		    System.out.println("Reprovado"); 
+        */
+
+        /*
+         //condição ternária
+         
+        int nota = 7;
+		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+		System.out.println(resultado);
+         */
+    }
+}
